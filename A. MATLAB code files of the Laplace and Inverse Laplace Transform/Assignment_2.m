24593c69300e05a6df59cd94332041747d8e4e66
Lemuel Ada <157497997+limwelwel@users.noreply.github.com>
2024-02-29 19:44:24 +0800

    Update Assignment_2.m

--- A. MATLAB code files of the Laplace and Inverse Laplace Transform/Assignment_2.m
+++ A. MATLAB code files of the Laplace and Inverse Laplace Transform/Assignment_2.m
@@ -13,6 +13,6 @@ F2 = (5*(s +2)) / (s^2*(s + 1)*(s + 3));
 f2 = ilaplace(F2);
 pretty(f2)
 
-F3 = ((s^4 + 2*s^3 + 3*s^2 + 4*s + 5) / (s*(s + 1)));
+F3 = (s^4 + 2*s^3 + 3*s^2 + 4*s + 5) / (s*(s + 1));
 f3 = ilaplace(F3);
 pretty(f3)
